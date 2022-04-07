54e52587163f10d9a3c7f2a9c864c3443003c69d
Deniz Etkar <denizetkar@gmail.com>
2022-04-07 20:39:41 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'contacts' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts 2D_STRING_ARRAY queries as parameter.
+#
+
+
+class Node:
+    def __init__(self, val, terminal):
+        self.val = val
+        self.terminal = terminal
+        self.terminal_descendant_cnt = 0
+        self.children = {}
+
+    def _add_child(self, child):
+        self.children[child.val] = child
+        self.terminal_descendant_cnt += child.terminal_descendant_cnt + int(child.terminal)
+
+    def add_name(self, name):
+        if len(name) == 0:
+            return False
+        next_val = name[0]
+
+        if next_val not in self.children:
+            new_child = Node(next_val, len(name) == 1)
+            new_child.add_name(name[1:])
+            self._add_child(new_child)
+            return True
+
+        is_added = self.children[next_val].add_name(name[1:])
+        self.terminal_descendant_cnt += int(is_added)
+        return is_added
+
+    def get_node(self, name):
+        if len(name) == 0:
+            return self
+
+        next_val = name[0]
+        if next_val not in self.children:
+            return None
+        child = self.children[next_val]
+        return child.get_node(name[1:])
+
+
+class Trie:
+    def __init__(self):
+        self.roots = {}
+
+    def add(self, name):
+        root_val = name[0]
+        if root_val not in self.roots:
+            self.roots[root_val] = Node(root_val, len(name) == 1)
+        root = self.roots[root_val]
+        root.add_name(name[1:])
+
+    def get_node(self, name):
+        root_val = name[0]
+        if root_val not in self.roots:
+            return None
+        root = self.roots[root_val]
+        return root.get_node(name[1:])
+
+    def prefix_match_cnt(self, name):
+        node = self.get_node(name)
+        if node is None:
+            return 0
+        return node.terminal_descendant_cnt + int(node.terminal)
+
+
+def contacts(queries):
+    trie = Trie()
+    terminal_cnts = []
+    for query in queries:
+        if query[0] == "add":
+            name = query[1]
+            trie.add(name)
+        elif query[0] == "find":
+            name = query[1]
+            terminal_cnts.append(trie.prefix_match_cnt(name))
+    return terminal_cnts
+
+
+if __name__ == "__main__":
+    with open(os.environ["INPUT_PATH"], "r") as f, open(os.environ["OUTPUT_PATH"], "w") as fptr:
+        queries_rows = int(f.readline().strip())
+
+        queries = []
+
+        for _ in range(queries_rows):
+            queries.append(f.readline().rstrip().split())
+
+        result = contacts(queries)
+
+        fptr.write("\n".join(map(str, result)))
+        fptr.write("\n")
